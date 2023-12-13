77227404572c9f050d9f81a5fe7bcdf6b960700d
ArnoldsBerzins <arnoldsb3@gmail.com>
2023-12-13 18:30:30 +0200

    Movement animations

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -13,6 +13,10 @@ public partial class Player : CharacterBody2D
 
     public Consumable consumable = null;
 
+    // Animation
+    public Sprite2D sprite;
+    private AnimationPlayer animation;
+
     // Health bar
     private ProgressBar _healthBar;
     private Label label;
@@ -43,6 +47,8 @@ public partial class Player : CharacterBody2D
         camera = (Camera2D)GetNode("Camera2D");
         _healthBar = camera.GetNode<ProgressBar>("UI/HealthBar");
         label = camera.GetNode<Label>("UI/EmotionalDamage");
+        animation = GetNode<AnimationPlayer>("AnimationPlayer");
+        sprite = GetNode<Sprite2D>("Sprite");
         Health = 60f;
     }
 
@@ -56,11 +62,32 @@ public partial class Player : CharacterBody2D
 
         if (direction != Vector2.Zero)
         {
+            if (direction.Y < 0)
+            {
+                sprite.Scale = new Vector2(-1, sprite.Scale.Y);
+                animation.Play("RunUp");
+            } 
+            else if (direction.Y > 0) 
+            {
+                animation.Play("RunDown");
+                sprite.Scale = new Vector2(1, sprite.Scale.Y);
+            } 
+            else if (direction.X < 0)
+            {
+                animation.Play("Run");
+                sprite.Scale = new Vector2(-1, sprite.Scale.Y);
+            }
+            else if (direction.X > 0)
+            {
+                animation.Play("Run");
+                sprite.Scale = new Vector2(1, sprite.Scale.Y);
+            }
             velocity.X = direction.X * Speed;
             velocity.Y = direction.Y * Speed;
         }
         else
         {
+            animation.Play("Idle");
             velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
             velocity.Y = Mathf.MoveToward(velocity.Y, 0, Speed);
         }
